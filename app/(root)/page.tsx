49da3d6664594fecc584817c8b8795cb26d784d8
Alper Tanrıkulu <tanrikulualper77100@gmail.com>
2025-10-10 21:47:59 +0300

    Add homepage section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,48 @@
-import Header from "@/components/Header"
-import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
+  const scriptUrl = "https://s3.tradingview.com/external-embedding/embed-widget-";
   return (
     <div className="flex min-h-screen home-wrapper">
-      <Header />
-      Home
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl={`${scriptUrl}market-overview.js`}
+            className="custom-chart"
+            height={150}
+            config={MARKET_DATA_WIDGET_CONFIG}
+          />
+        </div>
+        <div className="md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl={`${scriptUrl}stock-heatmap.js`}
+            height={150}
+            config={HEATMAP_WIDGET_CONFIG}
+          />
+        </div>
+      </section>
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}timeline.js`}
+            className="custom-chart"
+            height={150}
+            config={TOP_STORIES_WIDGET_CONFIG}
+          />
+        </div>
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}market-quotes.js`}
+            height={150}
+            config={MARKET_DATA_WIDGET_CONFIG}
+          />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
